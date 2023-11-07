53048e6ec28f9842e209419a17a5c19e5aa01fb7
Nguyen Pham <nguyenfamj1409@gmail.com>
2023-11-07 23:15:13 +0200

    feat: new global api endpoint, updated types

--- apps/api/types/generated/components.d.ts
+++ apps/api/types/generated/components.d.ts
@@ -1,35 +1,172 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface MediaImageAndVideos extends Schema.Component {
-  collectionName: 'components_media_image_and_videos';
+export interface LayoutFooter extends Schema.Component {
+  collectionName: 'components_layout_footers';
   info: {
-    displayName: 'Image and Videos';
-    icon: 'landscape';
+    displayName: 'Footer';
+    description: '';
   };
   attributes: {
-    alt: Attribute.String;
-    images: Attribute.Media;
-    videos: Attribute.Media;
+    logo: Attribute.Component<'layout.logo'> & Attribute.Required;
+    menuLinks: Attribute.Component<'links.link', true> & Attribute.Required;
+    socialLinks: Attribute.Component<'links.social-links', true> &
+      Attribute.Required;
+    legalLinks: Attribute.Component<'links.link', true> & Attribute.Required;
   };
 }
 
-export interface SeoTag extends Schema.Component {
-  collectionName: 'components_seo_tags';
+export interface LayoutLogo extends Schema.Component {
+  collectionName: 'components_layout_logos';
   info: {
-    displayName: 'tag';
-    icon: 'priceTag';
+    displayName: 'logo';
   };
   attributes: {
-    name: Attribute.String & Attribute.Required;
-    slug: Attribute.String;
+    img: Attribute.Media & Attribute.Required;
+    text: Attribute.String;
+  };
+}
+
+export interface LayoutNavBar extends Schema.Component {
+  collectionName: 'components_layout_nav_bars';
+  info: {
+    displayName: 'NavBar';
+    icon: 'pinMap';
+    description: '';
+  };
+  attributes: {
+    mainHeaders: Attribute.Component<'links.link', true> & Attribute.Required;
+    logo: Attribute.Component<'layout.logo'>;
+    promobar: Attribute.Component<'layout.promo-bar'>;
+  };
+}
+
+export interface LayoutPromoBar extends Schema.Component {
+  collectionName: 'components_layout_promo_bars';
+  info: {
+    displayName: 'Promo Bar';
+    description: '';
+  };
+  attributes: {
+    text: Attribute.String & Attribute.Required;
+    url: Attribute.String;
+    openInNewTab: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface LinksLink extends Schema.Component {
+  collectionName: 'components_links_links';
+  info: {
+    displayName: 'link';
+    icon: 'link';
+    description: '';
+  };
+  attributes: {
+    url: Attribute.String & Attribute.Required;
+    openInNewTab: Attribute.Boolean & Attribute.DefaultTo<false>;
+    text: Attribute.String & Attribute.Required;
+  };
+}
+
+export interface LinksSocialLinks extends Schema.Component {
+  collectionName: 'components_links_social_links';
+  info: {
+    displayName: 'Social Links';
+    icon: 'apps';
+  };
+  attributes: {
+    url: Attribute.String & Attribute.Required;
+    openInNewTab: Attribute.Boolean & Attribute.DefaultTo<false>;
+    text: Attribute.String & Attribute.Required;
+    social_platform: Attribute.Enumeration<
+      [
+        'FACEBOOK',
+        'INSTAGRAM',
+        'SHOPEE',
+        'TIKTOK',
+        'YOUTUBE',
+        'TWITTER',
+        'WEBSITE'
+      ]
+    > &
+      Attribute.Required;
+  };
+}
+
+export interface MetaMetadata extends Schema.Component {
+  collectionName: 'components_meta_metadata';
+  info: {
+    displayName: 'metadata';
+    icon: 'archive';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    description: Attribute.Text & Attribute.Required;
+  };
+}
+
+export interface SharedMetaSocial extends Schema.Component {
+  collectionName: 'components_shared_meta_socials';
+  info: {
+    displayName: 'metaSocial';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    socialNetwork: Attribute.Enumeration<['Facebook', 'Twitter']> &
+      Attribute.Required;
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    description: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 65;
+      }>;
+    image: Attribute.Media;
+  };
+}
+
+export interface SharedSeo extends Schema.Component {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+  };
+  attributes: {
+    metaTitle: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaDescription: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 50;
+        maxLength: 160;
+      }>;
+    metaImage: Attribute.Media;
+    metaSocial: Attribute.Component<'shared.meta-social', true>;
+    keywords: Attribute.Text;
+    metaRobots: Attribute.String;
+    structuredData: Attribute.JSON;
+    metaViewport: Attribute.String;
+    canonicalURL: Attribute.String;
   };
 }
 
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'media.image-and-videos': MediaImageAndVideos;
-      'seo.tag': SeoTag;
+      'layout.footer': LayoutFooter;
+      'layout.logo': LayoutLogo;
+      'layout.nav-bar': LayoutNavBar;
+      'layout.promo-bar': LayoutPromoBar;
+      'links.link': LinksLink;
+      'links.social-links': LinksSocialLinks;
+      'meta.metadata': MetaMetadata;
+      'shared.meta-social': SharedMetaSocial;
+      'shared.seo': SharedSeo;
     }
   }
 }
