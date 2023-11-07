d5224a36d0bd39592323e5c0681e05f75bc42fe7
Nguyen Pham <nguyenfamj1409@gmail.com>
2023-11-07 23:21:13 +0200

    feat: move route to [lang] folder

---
+++ apps/client/app/[lang]/page.tsx
@@ -0,0 +1,9 @@
+export default function Page(): JSX.Element {
+  return (
+    <>
+      <section className='hero-section'>
+        <div>a</div>
+      </section>
+    </>
+  );
+}
--- apps/client/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page(): JSX.Element {
-  return <div>Main Page</div>;
-}
